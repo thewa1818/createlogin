6cae68860a52197e0e179fc04b08bfb8427c49e8
aizawa000 <aizawa333.18@gmail.com>
2024-03-28 08:58:26 +0900

    4

--- public/login.php
+++ public/login.php
@@ -1,10 +1,8 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://createlogin.vercel.app");
-    header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Allow-Headers: Content-Type");
-    exit;
-}
+
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
 header("Content-Type: application/json");
 
