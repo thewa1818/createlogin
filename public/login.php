6e8c8121374bb829475914ba030a70a50a5c9a43
aizawa000 <aizawa333.18@gmail.com>
2024-03-30 20:02:33 +0900

    a

--- public/login.php
+++ public/login.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://createlogin.vercel.app");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
